7b684ed5a9b95e22e1ed2ef48e3280db9f63b786
Emanuele Flebus <emanuele.flebus@gmail.com>
2022-06-03 20:30:06 +0200

    Class Population: added methods to compute fitness and weight of individuals

--- src/solution.py
+++ src/solution.py
@@ -9,7 +9,8 @@ class Individual:
     chromosome: np.ndarray = field(init=False)
 
     def __post_init__(self) -> None:
-        self.chromosome = np.zeros(self.chromosome_size, dtype=np.uint8)
+        self.chromosome = np.random.randint(
+            2, size=self.chromosome_size, dtype=np.uint8)
 
     def compute_fitness(self, items_profits: np.ndarray) -> int:
         return np.sum(self.chromosome * items_profits)
@@ -27,3 +28,9 @@ class Population:
     def __post_init__(self) -> None:
         self.individuals = [Individual(self.chromosome_size)
                             for _ in range(self.population_size)]
+
+    def compute_fitness(self, items_profits: np.ndarray) -> np.ndarray:
+        return np.array([individual.compute_fitness(items_profits) for individual in self.individuals])
+
+    def compute_weight(self, items_weights: np.ndarray) -> np.ndarray:
+        return np.array([individual.compute_weight(items_weights) for individual in self.individuals])
