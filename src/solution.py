00348cdd68d1418c564d5548f6895b1015b4c69c
Emanuele Flebus <emanuele.flebus@gmail.com>
2022-06-09 00:15:00 +0200

    Added convenience methods to classes Individual and Population

--- src/solution.py
+++ src/solution.py
@@ -1,49 +1,52 @@
+import copy
+
 import numpy as np
 
 
 class Individual:
     def __init__(self, chromosome: np.ndarray) -> None:
-        self.chromosome = chromosome
-        self.chromosome_size = len(chromosome)
+        self._chromosome = chromosome
+        self.num_genes = len(chromosome)
 
     @classmethod
-    def from_random_chromosome(cls, chromosome_size: int):
-        return cls(np.random.randint(2, size=chromosome_size, dtype=np.uint8))
+    def from_random_chromosome(cls, num_genes: int):
+        return cls(np.random.randint(2, size=num_genes, dtype=np.uint8))
 
-    def evaluate_attribute(self, items_property: np.ndarray) -> int:
-        return np.sum(self.chromosome * items_property)
+    @property
+    def chromosome(self) -> np.ndarray:
+        return self._chromosome
 
-    def mutate(self, p_mutation: float) -> None:
+    def mutation(self, p_mutation: float) -> None:
         mutation_idxs = np.random.random(
-            size=self.chromosome_size) <= p_mutation
-        self.chromosome[mutation_idxs] = 1 - self.chromosome[mutation_idxs]
+            size=self.num_genes) <= p_mutation
+        self._chromosome[mutation_idxs] = 1 - self._chromosome[mutation_idxs]
 
     def split_chromosome(self, split_idx: int) -> list[np.ndarray]:
-        return np.split(self.chromosome, [split_idx])
+        return np.split(self._chromosome, [split_idx])
 
     def __str__(self) -> str:
-        return f"{self.chromosome}"
+        return f"{self._chromosome}"
 
 
 class Population:
     def __init__(self, individuals: list[Individual]) -> None:
-        self.individuals = individuals
-        self.population_size = len(individuals)
+        self._individuals = individuals
+        self.size = len(individuals)
 
     @classmethod
-    def from_random_individuals(cls, population_size: int, chromosome_size: int):
-        return cls([Individual.from_random_chromosome(chromosome_size) for _ in range(population_size)])
+    def from_random_individuals(cls, size: int, num_genes: int):
+        return cls([Individual.from_random_chromosome(num_genes) for _ in range(size)])
 
     @classmethod
-    def from_mating(cls, individuals: list[Individual], p_mating: float):
+    def from_mating(cls, mating_pool: list[Individual], p_mating: float):
         offspring = []
-        num_individuals = len(individuals)
+        num_individuals = len(mating_pool)
 
-        for parent1, parent2 in zip(individuals, individuals[1:]):
+        for parent1, parent2 in zip(mating_pool, mating_pool[1:]):
             new_individual1, new_individual2 = parent1, parent2
 
             if np.random.random() <= p_mating:
-                cross_idx = np.random.randint(1, num_individuals - 1)
+                cross_idx = np.random.randint(1, num_individuals)
                 new_individual1, new_individual2 = cls.single_point_crossover(
                     new_individual1, new_individual2, cross_idx)
 
@@ -51,16 +54,25 @@ class Population:
 
         return cls(offspring)
 
-    def evaluate_attribute(self, items_property: np.ndarray) -> np.ndarray:
-        return np.array([individual.evaluate_attribute(items_property) for individual in self.individuals])
+    @property
+    def individuals(self) -> list[Individual]:
+        return self._individuals
 
-    def mutate(self, p_mutation: float) -> None:
-        for individual in self.individuals:
-            individual.mutate(p_mutation)
+    def get_individual(self, idx: int) -> Individual:
+        return self._individuals[idx]
+
+    def get_chromosomes(self) -> list[np.ndarray]:
+        return [individual.chromosome for individual in self._individuals]
+
+    def get_chromosome(self, idx: int) -> np.ndarray:
+        return self._individuals[idx].chromosome
+
+    def mutation(self, p_mutation: float) -> None:
+        for individual in self._individuals:
+            individual.mutation(p_mutation)
 
     @staticmethod
     def single_point_crossover(parent1: Individual, parent2: Individual, cross_idx: int) -> tuple[Individual, Individual]:
-        """Single-point crossover recombination."""
         chromo1_part1, chromo1_part2 = parent1.split_chromosome(cross_idx)
         chromo2_part1, chromo2_part2 = parent2.split_chromosome(cross_idx)
 
@@ -69,8 +81,5 @@ class Population:
 
         return Individual(new_chromo_A), Individual(new_chromo_B)
 
-    def fittest_individual(self, items_profit: np.ndarray) -> Individual:
-        return self.individuals[np.argmax(self.evaluate_attribute(items_profit))]
-
     def __str__(self) -> str:
-        return '\n'.join([f"Individual #{i}: {individual}" for i, individual in enumerate(self.individuals, start=1)])
+        return '\n'.join([f"Individual #{i}: {individual}" for i, individual in enumerate(self._individuals)])
--- tests/test_solution.py
+++ tests/test_solution.py
@@ -1,28 +1,25 @@
 import numpy as np
 from src.solution import Individual, Population
 
-np.random.seed(42)
+np.random.seed(42)  # For reproducibility
 
 
 def test_individual() -> None:
     chromosome = np.array([1, 0, 1, 0], dtype=np.uint8)
     individual = Individual(chromosome=chromosome)
-    random_individual = Individual.from_random_chromosome(chromosome_size=8)
+    random_individual = Individual.from_random_chromosome(num_genes=8)
 
     assert np.array_equal(individual.chromosome, chromosome)
     assert np.array_equal(random_individual.chromosome,
                           np.array([0, 0, 1, 1, 1, 1, 0, 1]))
 
-    assert individual.chromosome_size == 4
+    assert individual.num_genes == 4
     assert isinstance(random_individual, Individual)
     assert random_individual.chromosome.dtype == np.uint8
 
-    assert individual.evaluate_attribute(
-        items_property=np.array([5, 10, 0, 1])) == 5
-
-    individual.mutate(p_mutation=0.5)
+    individual.mutation(p_mutation=0.5)
     assert np.array_equal(individual.chromosome, np.array([1, 0, 1, 1]))
-    individual.mutate(p_mutation=1)
+    individual.mutation(p_mutation=1)
     assert np.array_equal(individual.chromosome, np.array([0, 1, 0, 0]))
 
     part1, part2 = random_individual.split_chromosome(split_idx=3)
@@ -37,7 +34,7 @@ def test_population() -> None:
     individual2 = Individual(chromosome=chromosome2)
     population = Population(individuals=[individual1, individual2])
 
-    assert population.population_size == 2
+    assert population.size == 2
     assert isinstance(population.individuals[0], Individual)
     assert np.array_equal(population.individuals[0].chromosome, chromosome1)
     assert np.array_equal(population.individuals[1].chromosome, chromosome2)
