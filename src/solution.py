c2a203c0ddd91489706666b424f2c9449857cb67
Emanuele Flebus <emanuele.flebus@gmail.com>
2022-06-09 23:56:12 +0200

    Fixed bug with Individual and Population init

--- src/solution.py
+++ src/solution.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Individual:
     def __init__(self, chromosome: np.ndarray) -> None:
-        self._chromosome = chromosome
+        self._chromosome = chromosome.copy()
         self.num_genes = len(chromosome)
 
     @classmethod
@@ -19,9 +19,6 @@ class Individual:
             size=self.num_genes) <= p_mutation
         self._chromosome[mutation_idxs] = 1 - self._chromosome[mutation_idxs]
 
-    def split_chromosome(self, split_idx: int) -> list[np.ndarray]:
-        return np.split(self._chromosome, [split_idx])
-
     def __eq__(self, other):
         return np.array_equal(self._chromosome, other._chromosome)
 
@@ -31,7 +28,8 @@ class Individual:
 
 class Population:
     def __init__(self, individuals: list[Individual]) -> None:
-        self._individuals = individuals
+        self._individuals = [Individual(individual.chromosome)
+                             for individual in individuals]
         self.size = len(individuals)
 
     @classmethod
@@ -44,16 +42,27 @@ class Population:
         num_individuals = len(mating_pool)
 
         for parent1, parent2 in zip(mating_pool, mating_pool[1:]):
-            new_individual1, new_individual2 = parent1, parent2
+            new_chromosome1, new_chromosome2 = parent1.chromosome, parent2.chromosome
 
             if np.random.random() <= p_mating:
                 cross_idx = np.random.randint(1, num_individuals)
-                new_individual1, new_individual2 = cls.single_point_crossover(
-                    new_individual1, new_individual2, cross_idx)
+                new_chromosome1, new_chromosome2 = cls.single_point_crossover(
+                    new_chromosome1, new_chromosome2, cross_idx)
 
-            offspring.extend((new_individual1, new_individual2))
+            offspring.extend((Individual(new_chromosome1),
+                             Individual(new_chromosome2)))
 
         return cls(offspring)
+
+    @staticmethod
+    def single_point_crossover(chromosome1: np.ndarray, chromosome2: np.ndarray, cross_idx: int) -> tuple[np.ndarray, np.ndarray]:
+        chromo1_part1, chromo1_part2 = np.split(chromosome1, [cross_idx])
+        chromo2_part1, chromo2_part2 = np.split(chromosome2, [cross_idx])
+
+        new_chromosome_A = np.concatenate((chromo1_part1, chromo2_part2))
+        new_chromosome_B = np.concatenate((chromo2_part1, chromo1_part2))
+
+        return new_chromosome_A, new_chromosome_B
 
     @property
     def individuals(self) -> list[Individual]:
@@ -65,16 +74,6 @@ class Population:
     def mutation(self, p_mutation: float) -> None:
         for individual in self._individuals:
             individual.mutation(p_mutation)
-
-    @staticmethod
-    def single_point_crossover(parent1: Individual, parent2: Individual, cross_idx: int) -> tuple[Individual, Individual]:
-        chromo1_part1, chromo1_part2 = parent1.split_chromosome(cross_idx)
-        chromo2_part1, chromo2_part2 = parent2.split_chromosome(cross_idx)
-
-        new_chromo_A = np.concatenate((chromo1_part1, chromo2_part2))
-        new_chromo_B = np.concatenate((chromo2_part1, chromo1_part2))
-
-        return Individual(new_chromo_A), Individual(new_chromo_B)
 
     def __str__(self) -> str:
         return '\n'.join([f"Individual #{i}: {individual}" for i, individual in enumerate(self._individuals)])
